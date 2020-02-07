7386fcb99614bddd6bb253002cc0ac957e28865f
Rob Holt <roholt@microsoft.com>
2020-02-07 14:58:53 -0800

    Replace old rule implementation

--- Rules/UseDeclaredVarsMoreThanAssignments.cs
+++ Rules/UseDeclaredVarsMoreThanAssignments.cs
@@ -112,166 +112,9 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
         /// <returns>An enumerable containing diagnostic records</returns>
         private IEnumerable<DiagnosticRecord> AnalyzeScriptBlockAst(ScriptBlockAst scriptBlockAst, string fileName)
         {
-            IEnumerable<Ast> assignmentAsts = scriptBlockAst.FindAll(testAst => testAst is AssignmentStatementAst, false);
-            IEnumerable<Ast> varAsts = scriptBlockAst.FindAll(testAst => testAst is VariableExpressionAst, true);
-            IEnumerable<Ast> varsInAssignment;
-
-            Dictionary<string, AssignmentStatementAst> assignmentsDictionary_OrdinalIgnoreCase = new Dictionary<string, AssignmentStatementAst>(StringComparer.OrdinalIgnoreCase);
-
-            string varKey;
-            bool inAssignment;
-
-            if (assignmentAsts == null)
-            {
-                yield break;
-            }
-
-            foreach (AssignmentStatementAst assignmentAst in assignmentAsts)
-            {
-                // Only checks for the case where lhs is a variable. Ignore things like $foo.property
-                VariableExpressionAst assignmentVarAst = assignmentAst.Left as VariableExpressionAst;
-
-                if (assignmentVarAst == null)
-                {
-                    // If the variable is declared in a strongly typed way, e.g. [string]$s = 'foo' then the type is ConvertExpressionAst.
-                    // Therefore we need to the VariableExpressionAst from its Child property.
-                    var assignmentVarAstAsConvertExpressionAst = assignmentAst.Left as ConvertExpressionAst;
-                    if (assignmentVarAstAsConvertExpressionAst != null && assignmentVarAstAsConvertExpressionAst.Child != null)
-                    {
-                        assignmentVarAst = assignmentVarAstAsConvertExpressionAst.Child as VariableExpressionAst;
-                    }
-                }
-
-                if (assignmentVarAst != null)
-                {
-                    // Ignore if variable is global or environment variable or scope is drive qualified variable
-                    if (!Helper.Instance.IsVariableGlobalOrEnvironment(assignmentVarAst, scriptBlockAst)
-                        && !assignmentVarAst.VariablePath.IsScript
-                        && assignmentVarAst.VariablePath.DriveName == null)
-                    {
-                        string variableName = Helper.Instance.VariableNameWithoutScope(assignmentVarAst.VariablePath);
-
-                        if (!assignmentsDictionary_OrdinalIgnoreCase.ContainsKey(variableName))
-                        {
-                            assignmentsDictionary_OrdinalIgnoreCase.Add(variableName, assignmentAst);
-                        }
-                    }
-                }
-            }
-
-            if (varAsts != null)
-            {
-                foreach (VariableExpressionAst varAst in varAsts)
-                {
-                    varKey = Helper.Instance.VariableNameWithoutScope(varAst.VariablePath);
-                    inAssignment = false;
-
-                    if (assignmentsDictionary_OrdinalIgnoreCase.ContainsKey(varKey))
-                    {
-                        varsInAssignment = assignmentsDictionary_OrdinalIgnoreCase[varKey].Left.FindAll(testAst => testAst is VariableExpressionAst, true);
-
-                        // Checks if this variableAst is part of the logged assignment
-                        foreach (VariableExpressionAst varInAssignment in varsInAssignment)
-                        {
-                            // Try casting to AssignmentStatementAst to be able to catch case where a variable is assigned more than once (https://github.com/PowerShell/PSScriptAnalyzer/issues/833)
-                            var varInAssignmentAsStatementAst = varInAssignment.Parent as AssignmentStatementAst;
-                            var varAstAsAssignmentStatementAst = varAst.Parent as AssignmentStatementAst;
-                            if (varAstAsAssignmentStatementAst != null)
-                            {
-                                if (varAstAsAssignmentStatementAst.Operator == TokenKind.Equals)
-                                {
-                                    if (varInAssignmentAsStatementAst != null)
-                                    {
-                                        inAssignment = varInAssignmentAsStatementAst.Left.Extent.Text.Equals(varAstAsAssignmentStatementAst.Left.Extent.Text, StringComparison.OrdinalIgnoreCase);
-                                    }
-                                    else
-                                    {
-                                        inAssignment = varInAssignment.Equals(varAst);
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                inAssignment = varInAssignment.Equals(varAst);
-                            }
-                        }
-
-                        if (!inAssignment)
-                        {
-                            assignmentsDictionary_OrdinalIgnoreCase.Remove(varKey);
-                        }
-
-                        // Check if variable belongs to PowerShell built-in variables
-                        if (Helper.Instance.HasSpecialVars(varKey))
-                        {
-                            assignmentsDictionary_OrdinalIgnoreCase.Remove(varKey);
-                        }
-                    }
-                }
-            }
-
-            AnalyzeGetVariableCommands(scriptBlockAst, assignmentsDictionary_OrdinalIgnoreCase);
-
-            foreach (string key in assignmentsDictionary_OrdinalIgnoreCase.Keys)
-            {
-                yield return new DiagnosticRecord(
-                    string.Format(CultureInfo.CurrentCulture, Strings.UseDeclaredVarsMoreThanAssignmentsError, key),
-                    assignmentsDictionary_OrdinalIgnoreCase[key].Left.Extent,
-                    GetName(),
-                    DiagnosticSeverity.Warning,
-                    fileName,
-                    key);
-            }
-        }
-
-        /// <summary>
-        /// Detects variables retrieved by usage of Get-Variable and remove those
-        /// variables from the entries in <paramref name="assignmentsDictionary_OrdinalIgnoreCase"/>.
-        /// </summary>
-        /// <param name="scriptBlockAst"></param>
-        /// <param name="assignmentsDictionary_OrdinalIgnoreCase"></param>
-        private void AnalyzeGetVariableCommands(
-            ScriptBlockAst scriptBlockAst,
-            Dictionary<string, AssignmentStatementAst> assignmentsDictionary_OrdinalIgnoreCase)
-        {
-            var getVariableCmdletNamesAndAliases = Helper.Instance.CmdletNameAndAliases("Get-Variable");
-            IEnumerable<Ast> getVariableCommandAsts = scriptBlockAst.FindAll(testAst => testAst is CommandAst commandAst &&
-                getVariableCmdletNamesAndAliases.Contains(commandAst.GetCommandName(), StringComparer.OrdinalIgnoreCase), true);
-
-            foreach (CommandAst getVariableCommandAst in getVariableCommandAsts)
-            {
-                var commandElements = getVariableCommandAst.CommandElements.ToList();
-                // The following extracts the variable name(s) only in the simplest possible usage of Get-Variable.
-                // Usage of a named parameter and an array of variables is accounted for though.
-                if (commandElements.Count < 2 || commandElements.Count > 3) { continue; }
-
-                var commandElementAstOfVariableName = commandElements[commandElements.Count - 1];
-                if (commandElements.Count == 3)
-                {
-                    if (!(commandElements[1] is CommandParameterAst commandParameterAst)) { continue; }
-                    // Check if the named parameter -Name is used (PowerShell does not need the full
-                    // parameter name and there is no other parameter of Get-Variable starting with n).
-                    if (!commandParameterAst.ParameterName.StartsWith("n", StringComparison.OrdinalIgnoreCase))
-                    {
-                        continue;
-                    }
-                }
-
-                if (commandElementAstOfVariableName is StringConstantExpressionAst constantExpressionAst)
-                {
-                    assignmentsDictionary_OrdinalIgnoreCase.Remove(constantExpressionAst.Value);
-                    continue;
-                }
-
-                if (!(commandElementAstOfVariableName is ArrayLiteralAst arrayLiteralAst)) { continue; }
-                foreach (var expressionAst in arrayLiteralAst.Elements)
-                {
-                    if (expressionAst is StringConstantExpressionAst constantExpressionAstOfArray)
-                    {
-                        assignmentsDictionary_OrdinalIgnoreCase.Remove(constantExpressionAstOfArray.Value);
-                    }
-                }
-            }
+            var visitor = new Visitor(this, fileName);
+            scriptBlockAst.Visit(visitor);
+            return visitor.GetDiagnostics();
         }
 
         private class Visitor : AstVisitor, IAstPostVisitHandler
@@ -334,6 +177,10 @@ namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.BuiltinRules
 
                 // See if we're leaving the context of the last scriptblock we entered
                 // and if so, pop it off and see if any of the variables in that scope went unused
+                //
+                // NOTE: We don't look up the stack for variables,
+                // since PowerShell has dynamic, and not lexical, scope;
+                // looking up the stack only happens at runtime, so it's something we can't analyze
                 if (_scriptBlockContext.Peek().Key == ast)
                 {
                     Dictionary<string, ExpressionAst> unusedVariables = _scriptBlockContext.Pop().Value;
