412c5ab3d5e6ebb4acda2ce977463af1122a0596
Rob Holt <roholt@microsoft.com>
2020-03-02 08:27:41 -0800

    Add file

---
+++ Engine/ParameterBindingSimulator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation.Language;
+using System.Text;
+
+namespace Microsoft.Windows.PowerShell.ScriptAnalyzer
+{
+    internal class ParameterBindingResult
+    {
+        public IReadOnlyList<ExpressionAst> PositionalParameters { get; }
+
+        public IReadOnlyDictionary<string, KeyValuePair<CommandParameterAst, ExpressionAst>> NamedParameters { get; }
+    }
+
+    internal interface IParameterBindingSimulator
+    {
+
+    }
+}
